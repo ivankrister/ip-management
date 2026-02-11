fce45de0511782ce12aadc82c7380a5de9ac4ae3
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2026-02-11 21:27:15 +0800

    fix(auth): users controller return type

--- auth-service/app/Http/Controllers/UserController.php
+++ auth-service/app/Http/Controllers/UserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class UserController
 {
-    public function show()
+    public function show(): JsonResponse
     {
         return response()->json([
             'user' => Auth::user(),
