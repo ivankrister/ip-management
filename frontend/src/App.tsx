ba61ca94a71d1478e82a3a4dd03b292d7e34153c
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2026-02-15 12:01:59 +0800

    feat(frontend): added login ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { ComponentExample } from "@/components/component-example";
+import Login from "./pages/auth/Login";
 
 export function App() {
-return <ComponentExample />;
+return <Login />;
 }
 
 export default App;
